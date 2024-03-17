01999ad767a7f976cf9268b587f03af45926d85e
黄梓通 <12012710@mail.sustech.edu.cn>
2024-03-17 10:10:27 +0800

    feat(rm_simulator_link): finish rm_simulation_link

--- src/rm_simulator_link/src/rm_simulator_link.cpp
+++ src/rm_simulator_link/src/rm_simulator_link.cpp
@@ -9,7 +9,7 @@
 
 namespace RMSimulatorLink
 {
-class RMSimulatorLink : public rclcpp::Node
+class SimulatorLinkNode : public rclcpp::Node
 {
 private:
     double yaw;
@@ -19,17 +19,15 @@ private:
     std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster;
 
 public:
-    RMSimulatorLink(const rclcpp::NodeOptions & options) : Node("rm_simulator_link", options)
+    SimulatorLinkNode(const rclcpp::NodeOptions & options) : Node("rm_simulator_link", options)
     {
         RCLCPP_INFO(get_logger(), "RMSimulatorLink has been started.");
-        joint_sub = this->create_subscription<sensor_msgs::msg::JointState>("/joint", 10, std::bind(&RMSimulatorLink::joint_callback, this, std::placeholders::_1));
+        joint_sub = this->create_subscription<sensor_msgs::msg::JointState>("/joint", 10, std::bind(&SimulatorLinkNode::joint_callback, this, std::placeholders::_1));
         tf_broadcaster = std::make_unique<tf2_ros::TransformBroadcaster>(*this);
     }
 
     void joint_callback(const sensor_msgs::msg::JointState::SharedPtr msg)
     {
-        RCLCPP_INFO(get_logger(), "Received joint state message");
-
         yaw = msg->position[0];
         pitch = msg->position[1];
         geometry_msgs::msg::TransformStamped t;
@@ -44,3 +42,6 @@ public:
 };
 
 }  // namespace RMSimulatorLinks
+
+#include "rclcpp_components/register_node_macro.hpp"
+RCLCPP_COMPONENTS_REGISTER_NODE(RMSimulatorLink::SimulatorLinkNode)
